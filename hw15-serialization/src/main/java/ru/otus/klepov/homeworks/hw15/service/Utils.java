f9f92acc942c18dcecc858a9f73be98485f63397
Клепов Алексей Геннадиевич <AGKlepov@mtsbank.ru>
2023-10-24 17:06:33 +0300

    readfile to varv try

--- hw15-serialization/src/main/java/ru/otus/klepov/homeworks/hw15/service/Utils.java
+++ hw15-serialization/src/main/java/ru/otus/klepov/homeworks/hw15/service/Utils.java
@@ -3,6 +3,7 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import com.google.protobuf.Timestamp;
 import io.vavr.control.Either;
+import io.vavr.control.Try;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
@@ -16,12 +17,7 @@ import static ru.otus.klepov.homeworks.hw15.service.InstantTypeAdapter.DATE_PATT
 
 public class Utils {
     public static Either<Throwable, String> readFile(String fileName) {
-        try {
-            return Either.right(Resources.toString(Resources.getResource(fileName), Charsets.UTF_8));
-        }
-        catch(Exception ex) {
-            return Either.left(ex);
-        }
+        return Try.of(() -> Resources.toString(Resources.getResource(fileName), Charsets.UTF_8)).toEither();
     }
     public static Instant parseString2Instant(String date, String zone, String pattern) {
         SimpleDateFormat df = new SimpleDateFormat(DATE_PATTERN);
