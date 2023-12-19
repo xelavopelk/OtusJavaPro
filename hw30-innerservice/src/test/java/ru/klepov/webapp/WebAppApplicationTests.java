16bbf36cd6d672269694c10efc02693cedcc38ad
Alex Klepov <alexezm@yandex.ru>
2023-12-19 21:03:28 +0300

    init

---
+++ hw30-innerservice/src/test/java/ru/klepov/webapp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.klepov.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
